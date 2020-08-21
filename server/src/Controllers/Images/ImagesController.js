435ed071425fa90cbcb3cc47891e42606aeba2ed
Adisak <adisak0661013@gmail.com>
2020-08-21 16:34:04 +0700

    Add import images service

--- server/src/Controllers/Images/ImagesController.js
+++ server/src/Controllers/Images/ImagesController.js
@@ -1,4 +1,9 @@
-import { APP, ENDPOINT } from '../../MainProperty';
+import { APP, ENDPOINT, ROOT_URL } from '../../MainProperty';
+
+import ResultDataProperty from '../../BusinessData/Result/ResultData/ResultDataProperty';
+import ResponseProperty from '../../BusinessData/Response/ResponseProperty';
+
+import Model from '../../Models/Images/ImagesModel';
 
 class ImagesController {
     async setController() {
@@ -8,6 +13,22 @@ class ImagesController {
                 res.redirect(ENDPOINT + '/image' + '/' + params.group + '/' + params.id + '.jpg');
             } catch (err) {
                 res.sendStatus(500).end();
+            }
+        });
+
+        APP.post(ROOT_URL + '/image/import', async function (req, res) {
+            try {
+                const log = res.log;
+                const params = req.body;
+                const model = new Model();
+                const resultData = await model.import(log, params);
+                const response = new ResponseProperty();
+                response.submit(res, resultData);
+            } catch (err) {
+                const resultData = new ResultDataProperty();
+                await resultData.systemError({ developerMoreInfo: err.message });
+                const response = new ResponseProperty();
+                response.submit(res, resultData, err);
             }
         });
     }
